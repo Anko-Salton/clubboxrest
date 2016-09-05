65079a40591a02ebda3e8cd6fc17c8a8b74c2327
THIMOTHEE <THIMOTHEE@Timothee-PC>
2016-09-05 17:02:32 +0200

    TestController

--- clubbox-server/src/com/clubboxrest/controller/TestController.java
+++ clubbox-server/src/com/clubboxrest/controller/TestController.java
@@ -2,8 +2,10 @@ package com.clubboxrest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.clubboxrest.model.User;
 import com.clubboxrest.model.mapper.UserInterface;
@@ -14,11 +16,13 @@ public class TestController {
 	@Autowired
 	private UserInterface userInterface;
 	
-	@RequestMapping("test")
-	@ResponseBody
-	public String test(){
-		User a = userInterface.findOne(1); 
-		return a.getName();
+	@RequestMapping(value="/user",produces="application/json")
+	public @ResponseBody Iterable<User> allUser(){
+		return userInterface.findAll();
+	}
+	@RequestMapping(value = "/user/{id}", produces="application/json")
+	public @ResponseBody User aUser(@PathVariable Integer id){
+		return  userInterface.findOne(id); 
 	}
 	
 
